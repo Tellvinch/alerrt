7e8d0e8e5ffb6b3e9265f0e195c5be75352f62fb
Tellvinch <tellvinchimani@gmail.com>
2020-06-01 10:55:49 +0300

    add changes

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
+[![codebeat badge](https://codebeat.co/badges/3eca6d6c-6571-4512-b4fc-3f7ab6211ab8)](https://codebeat.co/projects/github-com-tellvinch-newsalert-master)
 # newsalert
-a web application that provdes the user with the latest trending news with receipts.
+a web application that provides the user with the latest trending news with receipts.
 
 ## Setup/Installation Requirements
 * follow this [link](https://github.com/Tellvinch/newsalert)
--- app/views.py
+++ app/views.py
@@ -1,16 +1,21 @@
 from flask import render_template
 from app import app
+from .request import get_news
 
-# Views
+
 @app.route('/')
 def index():
 
     '''
-    View root page function that returns the news page and its data
+    View root page function that returns the index page and its data
     '''
 
-    title = 'Home - Welcome to the most trusted news website online.'
-    return render_template('news.html', title = title)
+    # Getting popular movie
+    trending_news = get_news('name')
+    print(trending_news)
+    title = 'Home - Welcome to The bestNews Website Online'
+    return render_template('index.html', title = title,popular = trending_news)
+
 
 @app.route('/news/<int:news_id>')
 def news(news_id):
