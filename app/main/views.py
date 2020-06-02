96f8d841b4091a61d36f1a7095735844e1c93e38
Tellvinch <tellvinchimani@gmail.com>
2020-06-02 07:39:02 +0300

    add changes

---
+++ app/main/views.py
@@ -0,0 +1,30 @@
+from flask import render_template, request, redirect, url_for
+from . import main
+from ..requests import get_news, get_news
+# from .forms import ReviewForm
+from ..models import Description
+
+# ...
+# Views
+
+
+@main.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting popular movie
+    trending_news = get_news('name')
+    print(trending_news)
+    title = 'Home - Welcome to The bestNews Website Online'
+    return render_template('index.html', title=title, popular=trending_news)
+
+
+@main.route('/news/<int:news_id>')
+def news(news_id):
+    '''
+    View news page function that returns the news details page and its data
+    '''
+    title = f'You are viewing {news_id}'
+    return render_template('news.html', title=title)
