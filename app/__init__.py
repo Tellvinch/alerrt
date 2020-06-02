637250b872693baf9abf450e4a847e25a3334d5f
Tellvinch <tellvinchimani@gmail.com>
2020-06-02 07:31:13 +0300

    add changes

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from .config import DevConfig
 
 
 # Initializing application
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config = True)
 
 # Setting up configuration
 app.config.from_object(DevConfig)
--- app/config.py
+++ app/config.py
@@ -2,7 +2,9 @@ class Config:
     '''
     General configuration parent class
     '''
-    NEWS_API_BASE_URL ='https://newsapi.org/3/news/{}?api_key={}'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+
+
 
 
 
