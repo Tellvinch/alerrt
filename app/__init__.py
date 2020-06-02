a53ca3da531341f6dd8136b9fd9dda1bb59f24b5
Tellvinch <tellvinchimani@gmail.com>
2020-06-02 07:37:22 +0300

    add changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,25 @@
 from flask import Flask
-from .config import DevConfig
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
 
 
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
+def create_app(config_name):
+    app = Flask(__name__)
 
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile('config.py')
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
 
+    # Initializing flask extensions
+    bootstrap.init_app(app)
 
-from app import views
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # setting config
+    from .requests import configure_request
+    configure_request(app)
+
+    return app
--- app/models.py
+++ app/models.py
@@ -11,3 +11,35 @@ class News:
         self.description= description
         self.publishedAt=publishedAt
         
+
+class Description:
+
+    all_descriptions = []
+
+    def __init__(self,id,name,Author,title,description,publishedAt):
+        self.id =id
+        self.name = name
+        self.Author = Author
+        self.title= title
+        self.description= description
+        self.publishedAt=publishedAt
+
+
+    def save_description(self):
+        Description.all_descriptions.append(self)
+
+
+    @classmethod
+    def clear_description(cls):
+        Description.all_descriptions.clear()
+
+    @classmethod
+    def get_description(cls,id):
+
+        response = []
+
+        for description in cls.all_descriptions:
+            if description.movie_id == id:
+                response.append(description)
+
+        return response
